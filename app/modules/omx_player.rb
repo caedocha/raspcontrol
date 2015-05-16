e80101941067f5b72e3a95d322033e9e6594dc46
carlos <caedo.00@gmail.com>
2015-05-16 15:06:56 -0600

    Add DISPLAY var to system calls

--- app/modules/omx_player.rb
+++ app/modules/omx_player.rb
@@ -7,7 +7,7 @@ class OMXPlayer
   }
 
   def self.launch(video)
-    command = "xterm -e 'xterm -fullscreen -fg black -bg black -e omxplayer -o hdmi #{video}' &"
+    command = "#{set_env_display} && xterm -e 'xterm -fullscreen -fg black -bg black -e omxplayer -o hdmi #{video}' &"
     fork do
       exec command
     end
@@ -15,35 +15,39 @@ class OMXPlayer
 
   def self.play_pause
     omx_window = WindowInfo.get_omx_info
-    `xdotool windowactivate #{omx_window} && xdotool key p`
+    `#{set_env_display} && xdotool windowactivate #{omx_window} && xdotool key p`
   end
 
   def self.quit
     omx_window = WindowInfo.get_omx_info
-    `xdotool windowactivate #{omx_window} && xdotool key q`
+    `#{set_env_display} && xdotool windowactivate #{omx_window} && xdotool key q`
   end
 
   def self.increase_volume
     omx_window = WindowInfo.get_omx_info
-    `xdotool windowactivate #{omx_window} && xdotool key plus`
+    `#{set_env_display} && xdotool windowactivate #{omx_window} && xdotool key plus`
   end
 
   def self.decrease_volume
     omx_window = WindowInfo.get_omx_info
-    `xdotool windowactivate #{omx_window} && xdotool key minus`
+    `#{set_env_display} && xdotool windowactivate #{omx_window} && xdotool key minus`
   end
 
   def self.seek_forward
     omx_window = WindowInfo.get_omx_info
-    `xdotool windowactivate #{omx_window} && xdotool key Right`
+    `#{set_env_display} && xdotool windowactivate #{omx_window} && xdotool key Right`
   end
 
   def self.seek_backward
     omx_window = WindowInfo.get_omx_info
-    `xdotool windowactivate #{omx_window} && xdotool key Left`
+    `#{set_env_display} && xdotool windowactivate #{omx_window} && xdotool key Left`
   end
 
   private
+
+  def set_env_display
+    'export DISPLAY=:0'
+  end
 
   def exec_command_on_window
   end
