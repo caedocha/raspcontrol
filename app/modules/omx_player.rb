ff3eada2cb0eee8b02579da3197a2b309d8eb09b
carlos <caedo.00@gmail.com>
2015-05-15 18:33:55 -0600

    All omx commands use omx window

--- app/modules/omx_player.rb
+++ app/modules/omx_player.rb
@@ -19,26 +19,34 @@ class OMXPlayer
   end
 
   def self.quit
-    `xdotool key q`
+    omx_window = WindowInfo.get_omx_info
+    `xdotool windowactivate #{omx_window} && xdotool key q`
   end
 
   def self.increase_volume
-    `xdotool key plus`
+    omx_window = WindowInfo.get_omx_info
+    `xdotool windowactivate #{omx_window} && xdotool key plus`
   end
 
   def self.decrease_volume
-    `xdotool key minus`
+    omx_window = WindowInfo.get_omx_info
+    `xdotool windowactivate #{omx_window} && xdotool key minus`
   end
 
   def self.seek_forward
-    `xdotool key Right`
+    omx_window = WindowInfo.get_omx_info
+    `xdotool windowactivate #{omx_window} && xdotool key Right`
   end
 
   def self.seek_backward
-    `xdotool key Left`
+    omx_window = WindowInfo.get_omx_info
+    `xdotool windowactivate #{omx_window} && xdotool key Left`
   end
 
   private
+
+  def exec_command_on_window
+  end
 
   def is_xdotool_installed?
   end
