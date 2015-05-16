f086d1a178b649bcd60a095cae9f8d5906f83e88
carlos <caedo.00@gmail.com>
2015-05-16 16:29:03 -0600

    Kill omx processes after quitting

--- app/modules/env_utils.rb
+++ app/modules/env_utils.rb
@@ -1,5 +1,19 @@
 module EnvUtils
+
   def self.set_env_display
     'export DISPLAY=:0'
   end
+
+  def self.kill_all_omx_processes
+    pids = `ps aux | grep omxplayer | awk '{print $2}'`.split("\n")
+    pids.each do |pid|
+      begin
+        Process.kill('KILL', pid.to_i)
+      rescue
+        Rails.logger.info("Could not kill process: #{pid}")
+        Rails.logger.info("Process info: #{`ps -p #{pid} -o comm=`}")
+      end
+    end
+  end
+
 end
